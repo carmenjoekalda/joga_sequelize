69e75728d7f3ab4ac10a215dff8f32bd9d529045
carmenjoekalda <cjoekalda@gmail.com>
2024-02-14 10:42:16 +0200

    Created article table and added table data

---
+++ migrations/20240207091218-modify_article_fields.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const { query } = require("express");
+
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.changeColumn('articles', 'id', {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      }),
+      queryInterface.changeColumn('articles', 'name', {
+        type: Sequelize.STRING,
+        allowNull: false
+      }),
+      queryInterface.changeColumn('articles', 'slug', {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      }),
+      queryInterface.changeColumn('articles', 'image', {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('articles', 'body', {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('articles', 'published', {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }),
+      queryInterface.changeColumn('articles', 'author_id', {
+        type: Sequelize.INTEGER,
+      }),
+    ])
+  },
+
+down(queryInterface, Sequelize) {
+  return Promise.all([
+    queryInterface.dropTable('articles')
+  ])
+  }
+};
