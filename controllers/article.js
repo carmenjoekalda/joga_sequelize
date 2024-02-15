58caea57e78aafc088354a3c51e5edc82399c640
carmenjoekalda <cjoekalda@gmail.com>
2024-02-15 08:52:40 +0200

    Got author articles data

--- controllers/article.js
+++ controllers/article.js
@@ -9,13 +9,13 @@ const models = require('../models')
 // get all data from table
 const getAllArticles = (req, res) => {
     models.Article.findAll()
-    .then(articles => {
-        console.log(articles)
-        return res.status(200).json({articles});
-    })
-    .catch (error => {
-        return res.status(500).send(error.message);
-    })
+        .then(articles => {
+            console.log(articles)
+            return res.status(200).json({ articles });
+        })
+        .catch(error => {
+            return res.status(500).send(error.message);
+        })
 };
 
 // show article by slug
@@ -28,17 +28,36 @@ const getArticleBySlug = (req, res) => {
             model: models.Author
         }],
     })
-    .then(article => {
-        console.log(article)
-        return res.status(200).json({article});
+        .then(article => {
+            console.log(article)
+            return res.status(200).json({ article });
+        })
+        .catch(error => {
+            return res.status(500).send(error.message);
+        })
+};
+
+// show articles by author id
+const getArticleByAuthor = (req, res) => {
+    models.Article.findAll({
+        where: {
+            author_id: req.params.id
+        },
+        include: [{
+            model: models.Author
+        }]
     })
-    .catch (error => {
-        return res.status(500).send(error.message);
-    })
+        .then(articles => {
+            return res.status(200).json({ articles });
+        })
+        .catch(error => {
+            return res.status(500).send(error.message);
+        });
 };
 
 // export constroller functions
 module.exports = {
     getAllArticles,
-    getArticleBySlug
+    getArticleBySlug,
+    getArticleByAuthor
 };
--- index.js
+++ index.js
@@ -24,6 +24,7 @@ sequelize
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
 app.use('/article', articleRouter)
+app.use('/author', articleRouter)
 
 
 // listen requests
